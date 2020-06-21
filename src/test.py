d2de2aa7e1ce08ffb23cec2c8e78b4a50e6eb996
woodamor <tmacwood@qq.com>
2020-06-21 19:09:02 +0800

    nothing

---
+++ src/test.py
@@ -0,0 +1,7 @@
+# This Python file uses the following encoding: utf-8
+from PyQt5 import QtCore
+from PyQt5 import QtWidgets
+
+class test(QWidget):
+    def __init__(self):
+        QtWidgets.QWidget.__init__(self)
